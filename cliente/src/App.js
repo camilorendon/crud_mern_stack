f233bd043ed8e553ee775b605e328cb74a7cf6d4
camilorendon <juancamilo.g.0512@gmail.com>
2023-11-01 12:14:15 -0500

    feat: routes

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,15 +1,21 @@
-import './App.css';
-import ListUsers from './components/ListUsers';
-import AddUser from './components/AddUser';
-import EditUser from './components/EditUser';
+import "./App.css";
+import ListUsers from "./components/ListUsers";
+import AddUser from "./components/AddUser";
+import EditUser from "./components/EditUser";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <h1>hola mundo</h1>
-      <ListUsers />
-      <AddUser />
-      <EditUser />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ListUsers />} exact></Route>
+          <Route path="/agregar-usuario" element={<AddUser />} exact></Route>
+          <Route path="/editar-usuario" element={<EditUser />} exact></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
